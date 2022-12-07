c32647ff17e7bfd86518a6b6d227c2e0aeb76cdf
leonidcv <47688720+leonidcv@users.noreply.github.com>
2022-12-07 18:13:41 +0200

    Divided by functions

--- main.cpp
+++ main.cpp
@@ -5,6 +5,79 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+void checkAandB(double &a, double &b) {
+    do {
+
+        cout << "Please input 'a' and 'b' which are the range of 'x'. Remember! 'b' must be greater than 'a'" << endl;
+        cout << "a: ";
+
+        while(!(cin >> a)) {         // enter loop scope if "cin >> a" fails
+            if(cin.eof()) return;      // return if the stream was closed
+            cin.clear();                  // clear the stream state
+
+            // ignore everything until a newline char is found
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            cout << "Icorrect data! Please try again." << endl;
+            cout << "a: ";
+        }
+
+        cout << "b: ";
+
+        while(!(cin >> b)) {         // enter loop scope if "cin >> b" fails
+            if(cin.eof()) return;      // return if the stream was closed
+            cin.clear();                  // clear the stream state
+
+            // ignore everything until a newline char is found
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            cout << "Icorrect data! Please try again." << endl;
+            cout << "b: ";
+        }
+
+    } while(a > b);
+}
+
+void checkN(double &n) {
+    do {
+
+        cout << "Please input 'n'. Warning! 'n' must be greater than 2." << endl;
+        cout << "n: ";
+
+        while(!(cin >> n)) {         // enter loop scope if "cin >> n" fails
+            if(cin.eof()) return;      // return if the stream was closed
+            cin.clear();                  // clear the stream state
+
+            // ignore everything until a newline char is found
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            cout << "Icorrect data! Please try again." << endl;
+            cout << "n: ";
+        }
+
+    } while(n < 2 || !(n == (int)n));
+}
+
+void checkStep(double &step) {
+    do {
+
+        cout << "Please input step of this program. Remember! 'step' must be positive'." << endl;
+        cout << "step: ";
+
+        while(!(cin >> step)) {         // enter loop scope if "cin >> step" fails
+            if(cin.eof()) return;      // return if the stream was closed
+            cin.clear();                  // clear the stream state
+
+            // ignore everything until a newline char is found
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            cout << "Icorrect data! Please try again." << endl;
+            cout << "step: ";
+        }
+
+    } while(step <= 0);
+}
+
 double formula(double x, int n) {
 
     double result;
@@ -39,81 +112,20 @@ double formula(double x, int n) {
 
 }
 
+void range(double &a, double &b, double n, double step) {
+    for(a; a <= b; a = a + step) {
+        cout << "x = " << a << ", y = " << formula( a, n) + 100 << endl;
+    }
+}
+
 int main() {
     
     double a, b, n, step;
 
-    do {
-
-        cout << "Please input 'a' and 'b' which are the range of 'x'. Remember! 'b' must be greater than 'a'" << endl;
-        cout << "a: ";
-
-        while(!(cin >> a)) {         // enter loop scope if "cin >> a" fails
-            if(cin.eof()) return -1;      // return if the stream was closed
-            cin.clear();                  // clear the stream state
-
-            // ignore everything until a newline char is found
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-            cout << "Icorrect data! Please try again." << endl;
-            cout << "a: ";
-        }
-
-        cout << "b: ";
-
-        while(!(cin >> b)) {         // enter loop scope if "cin >> b" fails
-            if(cin.eof()) return -1;      // return if the stream was closed
-            cin.clear();                  // clear the stream state
-
-            // ignore everything until a newline char is found
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-            cout << "Icorrect data! Please try again." << endl;
-            cout << "b: ";
-        }
-
-    } while(a > b);
-
-    do {
-
-        cout << "Please input 'n'. Warning! 'n' must be greater than 2." << endl;
-        cout << "n: ";
-
-        while(!(cin >> n)) {         // enter loop scope if "cin >> n" fails
-            if(cin.eof()) return -1;      // return if the stream was closed
-            cin.clear();                  // clear the stream state
-
-            // ignore everything until a newline char is found
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-            cout << "Icorrect data! Please try again." << endl;
-            cout << "n: ";
-        }
-
-    } while(n < 2 || !(n == (int)n));
-
-    do {
-
-        cout << "Please input step of this program. Remember! 'step' must be positive'." << endl;
-        cout << "step: ";
-
-        while(!(cin >> step)) {         // enter loop scope if "cin >> step" fails
-            if(cin.eof()) return -1;      // return if the stream was closed
-            cin.clear();                  // clear the stream state
-
-            // ignore everything until a newline char is found
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-            cout << "Icorrect data! Please try again." << endl;
-            cout << "step: ";
-        }
-
-    } while(step <= 0);
-    
-    for(a; a <= b; a = a + step) {
-        cout << "x = " << a << ", y = ";
-        cout << formula( a, n) + 100 << endl;
-    }
+    checkAandB(a, b);
+    checkN(n);
+    checkStep(step);
+    range(a, b, n, step);
 
     return 0;
 
